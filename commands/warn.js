1e7391f218e32bcd0f80fa45690f5652aa745321
Aidan Nellyer <daleagaming@gmail.com>
2018-11-21 18:32:37 +0000

    Added logging system, cleaned up code.

--- commands/warn.js
+++ commands/warn.js
@@ -1,9 +1,14 @@
-const config = require("../config.json")
 exports.run = (client, message, [mention, ...reason]) => {
-    let moderation = config.moderation;
-    const modRole = message.guild.roles.find("name", `${moderation}`);
+    const config = require("../config.json")
+    const modRole = message.guild.roles.find("name", `${config.moderation}`);
+    let channel = client.channels.get(config.logs);
+    let date = new Date();
+    let warnMember = message.mentions.members.first();
+    let reasonMsg = reason.join(" ");
+    let warner = message.author.tag;
+
     if (!modRole)
-        return console.log(`The ${moderation} role does not exist`);
+        return console.log(`The ${config.moderation} role does not exist`);
 
     if (!message.member.roles.has(modRole.id))
         return message.reply("You can't use this command.");
@@ -14,32 +19,36 @@ exports.run = (client, message, [mention, ...reason]) => {
     if (!message.guild.me.hasPermission("MANAGE_MESSAGES"))
         return message.reply("");
 
-    let warnMember = message.mentions.members.first();
-    let reasonMsg = reason.join(" ");
 
-    let warner = message.author.tag;
 
     warnMember.send(`You've been warned for ${reasonMsg} by ${warner}`).then(member => {
-        let date = new Date();
 
-        message.reply({
-            embed: {
-                color: 13632027,
-                author: {
-                    icon_url: client.user.avatarURL
 
-                },
 
-                title: "A user has been warned!",
-                description: `A user was warned recently! Here is all the information you need
-            
-                **User Warned:** ${warnMember}
-                **Warned by:** ${warner}
-                **User was warned for:** ${reasonMsg}
-                **User was warned on:** ${date}`
+        if (channel) {
+            channel.send({
+                embed: {
+                    color: 13632027,
+                    author: {
+                        icon_url: client.user.avatarURL
 
-            }
+                    },
 
-        })
+                    title: "A user has been warned!",
+                    description: `A user was warned recently! Here is all the information you need
+        
+            **User Warned:** ${warnMember}
+            **Warned by:** ${warner}
+            **User was warned for:** ${reasonMsg}
+            **User was warned on:** ${date}`
+
+                }
+
+            })
+        }
     })
 };
+
+
+
+
