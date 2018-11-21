18b334366ceeee8178198b31565c72785d894606
Aidan Nellyer <daleagaming@gmail.com>
2018-11-21 18:41:23 +0000

    Logging functionality, cleaned up code

--- commands/ban.js
+++ commands/ban.js
@@ -2,6 +2,12 @@ const config = require("../config.json")
 exports.run = (client, message, [mention, ...reason]) => {
   let moderation = config.moderation;
   const modRole = message.guild.roles.find("name", `${moderation}`);
+  let banMember = message.mentions.members.first();
+  let reasonMsg = reason.join(" ");
+  let banner = message.author.tag;
+  let date = new Date();
+  let channel = client.channels.get(config.logs);
+
   if (!modRole)
     return console.log(`The ${moderation} role does not exist`);
 
@@ -14,32 +20,33 @@ exports.run = (client, message, [mention, ...reason]) => {
   if (!message.guild.me.hasPermission("BAN_MEMBERS"))
     return message.reply("");
 
-  let banMember = message.mentions.members.first();
-  let reasonMsg = reason.join(" ");
+
 
   message.member.send(`You were banned by **${banner}** for the reason: **${reasonMsg}**`)
 
   banMember.ban(reasonMsg).then(member => {
-    let banner = message.author.tag;
-    let date = new Date();
 
-    message.reply({
-      embed: {
-        color: 13632027,
-        author: {
-          icon_url: client.user.avatarURL
 
-        },
+    if (channel) {
+      channel.send({
+        embed: {
+          color: 13632027,
+          author: {
+            icon_url: client.user.avatarURL
 
-        title: "A user has been banned!",
-        description: `A user was banned recently! Here is all the information you need
-            
-            **User Banned:** ${banMember}
-            **Banned by:** ${banner}
-            **User was banned for:** ${reasonMsg}
-            **User was banned on:** ${date}`
-      }
-    })
+          },
+
+          title: "A user has been banned!",
+          description: `A user was banned recently! Here is all the information you need
+              
+              **User Banned:** ${banMember}
+              **Banned by:** ${banner}
+              **User was banned for:** ${reasonMsg}
+              **User was banned on:** ${date}`
+        }
+      })
+
+    }
 
 
   })
