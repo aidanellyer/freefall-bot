b45638efc3c360654b65f32b7cfb85a40cbba89a
Aidan Nellyer <daleagaming@gmail.com>
2018-11-26 18:46:53 +0000

    Class Redo

--- commands/ping.js
+++
@@ -1,4 +0,0 @@
-exports.run = (client, message) => {
-            message.reply("Pong! :ping_pong:").then(msg => msg.delete(3000));
-            message.channel.send(new Date().getTime() - message.createdTimestamp + " ms").then(msg => msg.delete(3000));        
-    }
